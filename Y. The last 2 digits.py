1c510dc0624325c5fd7e6cb6c6af79c6397cc630
shoryataneja <shoryataneja5@gmail.com>
2025-09-25 11:28:09 +0530

    did some more questions today

---
+++ Y. The last 2 digits.py
@@ -0,0 +1,4 @@
+a,b,c,d = map(int,input().split())
+num = a*b*c*d 
+
+print(f"{num % 100:02d}")
---
+++ Z. Hard Compare.py
@@ -0,0 +1,9 @@
+import math   
+a, b, c, d = map(int, input().split())
+
+
+
+if b * math.log(a) > d * math.log(c):
+    print("YES")
+else:
+    print("NO")
